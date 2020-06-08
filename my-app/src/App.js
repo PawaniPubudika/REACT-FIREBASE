d2a0fac8b42da5b05264cc4ad17fd5b466532713
PawaniPubudika <pubudikalwp@gmail.com>
2020-06-08 10:26:55 +0530

    first commit-new

---
+++ my-app/src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import firebase from "./firebase.js";
+// import react from "react";
+
+
+function App() {
+
+    const [spells, setSpells] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+        const db = firebase.firestore();
+        const data = await db.collection("questions").get();
+  
+        
+        setSpells(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  
+      };
+  
+      fetchData();
+  
+    }, []);
+  
+
+    return (
+    
+        <ol>
+    
+          {spells.map((spell) => (
+              
+            <li key={spell.title}>
+                {console.log(spell.title)} 
+              <h3>{spell.title}</h3><br/>
+              <input type="radio" />{spell.option1}<br/>
+              <input type="radio" />{spell.option2}<br/>
+              <input type="radio" />{spell.option3}<br/>
+                {console.log(spell)} 
+             </li>
+    
+          ))}
+          <br/>
+              <button type='submit'>Submit</button>
+        </ol>
+    
+      );
+
+
+  
+}
+export default App;
+
