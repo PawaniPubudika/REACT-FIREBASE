5ca4e05a2661a88cc9113b67668666bba294eeb7
PawaniPubudika <pubudikalwp@gmail.com>
2020-06-09 14:40:58 +0530

    3rd commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,18 +6,37 @@ import firebase from "./firebase.js";
 function App() {
 
     const [spells, setSpells] = useState([]);
+    const [newarray, setNewArray] = React.useState([]);
     useEffect(() => {
         const fetchData = async () => {
         const db = firebase.firestore();
         const data = await db.collection("questions").get();
-        setSpells(data.docs.map((doc) => ({ ...doc.data()})));
+        setSpells(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
   
       };
     fetchData();
   
     }, []);
-  
 
+    function handleChange(e) {
+      console.log(e.target.value);
+      // console.log(e.target.name);
+      // var choices=[e.target.value];
+      // console.log(choices);
+      // console.log(choices[0],choices[1],choices[2],choices[3],choices[4]);
+      
+    }
+    
+    function dosubmit(){
+      var answers;
+      spells.map((spell) => (
+        <div key={spell.id}>
+        {console.log(spell.answer)}
+        </div>
+      ))
+    }
+  
+ 
     return (
   
         <ol>
@@ -26,14 +45,13 @@ function App() {
             <li key={spell.title}>
                 {console.log(spell)} 
               <h3>{spell.title}</h3><br/>
-              <input type="radio" />{spell.option1}<br/>
-              <input type="radio" />{spell.option2}<br/>
-              <input type="radio" />{spell.option3}<br/>
-             </li>
-    
+              <input type="radio" name ={spell.id} value={spell.option1} onChange={handleChange}/>{spell.option1}<br/>
+              <input type="radio" name ={spell.id} value={spell.option2} onChange={handleChange}/>{spell.option2}<br/>
+              <input type="radio" name ={spell.id} value={spell.option3} onChange={handleChange}/>{spell.option3}<br/>
+            </li>  
           ))}
           <br/>
-              <button type='submit'>Submit</button>
+              <button type='submit' onClick={dosubmit}>Submit</button>
         </ol>
     
       );
